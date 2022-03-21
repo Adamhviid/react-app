86eaf9f03e802cc1ecd887803ccfb9a9da067707
Adam <adam@webrick.com>
2022-03-21 11:40:33 +0100

    pricetool collapse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import Price from './components/Price'
+import PriceTool from './components/PriceTool';
 import Header from './components/Header';
 import About from './components/About';
 import Image from './components/Image';
 import PropertyData from './components/PropertyData';
+
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
         </Grid>
         <Grid item md={12}>
           <Price />
+        </Grid>
+        <Grid item md={12}>
+          <PriceTool />
         </Grid>
         <Grid item md={6} xs={12}>
           <About />
